2c710e3251ffaf66900bcf39a7ec912cf9fc233c
Zhanna Sarsenbayeva <zsarsenbayev@4180L-132627-M.local>
2019-02-14 14:31:47 +1100

    new info on log regression

--- Logistic_regression.R
+++ Logistic_regression.R
@@ -15,3 +15,26 @@ test <- cccc[801:889,]
 
 model <- glm(Survived ~.,family=binomial(link='logit'),data=train)
 summary(model)
+
+anova(model, test="Chisq")
+
+library(pscl)
+pR2(model)
+
+# predictive abilities of a model
+fitted.results <- predict(model,newdata=subset(test,select=c(2,3,4,5,6,7,8)),type='response')
+fitted.results <- ifelse(fitted.results > 0.5,1,0)
+
+misClasificError <- mean(fitted.results != test$Survived)
+print(paste('Accuracy',1-misClasificError))
+
+install.packages("ROCR")
+library(ROCR)
+p <- predict(model, newdata=subset(test,select=c(2,3,4,5,6,7,8)), type="response")
+pr <- prediction(p, test$Survived)
+prf <- performance(pr, measure = "tpr", x.measure = "fpr")
+plot(prf)
+
+auc <- performance(pr, measure = "auc")
+auc <- auc@y.values[[1]]
+auc
